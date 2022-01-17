18458ee52d296c6d9b82992a37f819b2f22cd1a6
tiffanydoh <97858409+tiffanydoh@users.noreply.github.com>
2022-01-16 16:20:18 -0800

    Add files via upload

---
+++ treasurehunt.js
@@ -0,0 +1,32 @@
+alert("I am connected")
+
+var foundBomb = Math.floor(Math.random() * 8)
+var foundBomb2 = Math.floor(Math.random() * 8)
+var foundTreasure = Math.floor(Math.random() * 8)
+var counter = 5
+//var randomNum = Math.floor(Math.random() * 8)
+const treasure = (id) => {
+  counter = counter - 1
+  console.log("id:", id)
+if(foundBomb === id){
+ alert("Oh no, found Bomb! Keep Trying!")
+document.getElementById(id).innerHTML = "&#x2620"
+} else if (foundTreasure === id) {
+  alert("Found Treasure!")
+ document.getElementById(id).innerHTML = "&#x1f308"
+} else if(foundBomb2 ===id){
+  alert("Oh no, found Bomb! Keep Trying!")
+ document.getElementById(id).innerHTML = "&#x2620"
+}
+} else {
+  alert("Nothing Here. Try again!")
+  document.getElementById(id).innerHTML = "&#x1f332"
+
+if (counter === 0) {
+  alert("Game Over!")
+}
+
+
+
+}
+}
