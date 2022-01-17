3cc290b39ba8346d4fef5940a9034a5027916243
tiffanydoh <97858409+tiffanydoh@users.noreply.github.com>
2022-01-16 16:34:37 -0800

    Update treasurehunt.js

--- treasurehunt.js
+++ treasurehunt.js
@@ -1,7 +1,6 @@
 alert("I am connected")
 
 var foundBomb = Math.floor(Math.random() * 8)
-var foundBomb2 = Math.floor(Math.random() * 8)
 var foundTreasure = Math.floor(Math.random() * 8)
 var counter = 5
 //var randomNum = Math.floor(Math.random() * 8)
@@ -14,10 +13,6 @@ document.getElementById(id).innerHTML = "&#x2620"
 } else if (foundTreasure === id) {
   alert("Found Treasure!")
  document.getElementById(id).innerHTML = "&#x1f308"
-} else if(foundBomb2 ===id){
-  alert("Oh no, found Bomb! Keep Trying!")
- document.getElementById(id).innerHTML = "&#x2620"
-}
 } else {
   alert("Nothing Here. Try again!")
   document.getElementById(id).innerHTML = "&#x1f332"
